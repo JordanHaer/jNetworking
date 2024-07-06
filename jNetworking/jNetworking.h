94882516b6efebbe78181664d74e2852f5ab3a6f
JordanHaer <jordanhaer@gmail.com>
2024-07-06 09:36:19 +0100

    Initial Commit

---
+++ jNetworking/jNetworking.h
@@ -0,0 +1,18 @@
+//
+//  jNetworking.h
+//  jNetworking
+//
+//  Created by Jordan Haer on 06/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for jNetworking.
+FOUNDATION_EXPORT double jNetworkingVersionNumber;
+
+//! Project version string for jNetworking.
+FOUNDATION_EXPORT const unsigned char jNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <jNetworking/PublicHeader.h>
+
+
